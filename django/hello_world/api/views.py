5f76181e24c28b983bd20367e760bd081d5c2530
Kyle Lawson <Kyle.Lawson7@yahoo.com>
2017-10-09 00:21:16 -0400

    new

---
+++ django/hello_world/api/views.py
@@ -0,0 +1,45 @@
+from rest_framework.generics import (
+    ListAPIView,
+    CreateAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView,
+    RetrieveUpdateAPIView
+)
+
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAdminUser,
+    IsAuthenticatedOrReadOnly,
+)
+
+# from videos.models import Video
+#
+# # from .permissions import IsOwnerOrReadOnly
+#
+# from .serializers import (
+#     VideoListSerializer,
+#     VideoCreateUpdateSerializer,
+#     VideoDetailSerializer,
+# )
+#
+#
+# class VideoListAPIView(ListAPIView):
+#     serializer_class = VideoListSerializer
+#     queryset = Video.objects.all()
+#
+# class VideoCreateAPIView(CreateAPIView):
+#     queryset = Video.objects.all()
+#     serializer_class = VideoCreateUpdateSerializer
+#     permission_classes = [IsAuthenticated]
+#
+#     def perform_create(self, serializer):
+#         serializer.save(
+#             user=self.request.user,
+#         )
+#
+# class VideoDetailAPIView(RetrieveAPIView):
+#     queryset = Video.objects.all()
+#     serializer_class = VideoDetailSerializer
+#     lookup_field = 'unique_id'
